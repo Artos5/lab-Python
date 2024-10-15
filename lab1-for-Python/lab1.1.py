7338d80051182787428781386978cae35fe22bf4
Artos5 <33461064+Artos5@users.noreply.github.com>
2024-10-15 17:14:10 +0300

    Update lab1.1.py

--- lab1-for-Python/lab1.1.py
+++ lab1-for-Python/lab1.1.py
@@ -1,1 +1,25 @@
+a = int(input ("Введіть а: "))
 
+while (a < 1 or a > 100):
+
+    a = int(input ("Невірне число a!\r\nВведіть ще раз а: "))
+
+b = int(input ("Введіть b: "))
+
+while (b < 1 or b > 100):
+
+    b = int(input ("Невірне число b!\r\nВведіть ще раз b: "))
+
+if a > b:
+
+    r = a / b + 1
+
+elif a == b:
+
+    r = -2
+
+else:
+
+    r = (a - b) / a
+
+print("Результат обчислення виразу: " , r)
