6c0035f707184cdb06b45b92cb54f6a948591c47
Artos5 <33461064+Artos5@users.noreply.github.com>
2024-11-15 10:24:45 +0200

    Delete lab4 directory

--- lab4/lab4.1.py
+++
@@ -1,9 +0,0 @@
-n = int(input("n = "))
-s = 0
-print(f"Enter {n} array elements:")
-
-arr = [int(input()) for _ in range(n)]
-for i in range(len(arr)):  # цикл по кількості елементів списку
- if(arr[i]>0):
-  s+=arr[i]
-print("Сума додатніх чисел: ", s)
